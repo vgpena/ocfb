720075793f665dbdba46f710d1a7858ba2d01823
Violet Peña <vpena@oberlin.edu>
2012-04-05 23:23:32 -0400

    Header and footer, syntactified.

--- footer.php
+++ footer.php
@@ -1,10 +1,7 @@
-<br>
-                <p class="footer">
-                    <a href="siteinfo.php">Site Info</a> &bull; <a href="updates.php">Updates</a> &bull; <a href="http://oberlin.edu" target="blank">Oberlin</a> &bull; <a href="contact.php">Contact</a> &bull; <a href="shop.php">Shop</a>
-                </p>
-            </div>
-            <br>
-            <br>
-        </div>
-    </body>
+	</article>
+		<footer>
+			<a href="siteinfo.php">Site Info</a> &bull; <a href="updates.php">Updates</a> &bull; <a href="http://oberlin.edu" target="blank">Oberlin</a> &bull; <a href="contact.php">Contact</a> &bull; <a href="shop.php">Shop</a>
+			</footer>
+		</article>
+	</body>
 </html>
--- header.php
+++ header.php
@@ -1,246 +1,44 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE html>
     <head>
         <title>OCFB ONLINE: CONVOLUTI APIBUS</title>
-        <meta name="description" content="The official website of the Oberlin College fencing team, the Flaming Blades. Find out more about us or joining the team!"/>
-        <meta name="keywords" content="fencing,college,collegiate,Oberlin College,Oberlin,flaming,blades,flaming blades,OCFB,Ohio,USFA,foil,saber,epee,Walt Dragonetti"/>
-        <meta name="author" content="Violet Pe&ntilde;a"/>
-        <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1" />
-        <style type="text/css">
-            body {
-                background-color: #D6D6D6;
-                margin: auto;
-            }
-            
-            #site {
-                margin: auto;
-                width: 870px;
-            }
-            
-            #header {
-                margin: auto;
-                width: 870px;
-            }
-            
-            #headerbg {
-                width: 870px;
-                height: 355px;
-                padding-top: 20px;
-            }
-            
-            #headerfg {
-                width: 679px;
-                height: 477px;
-                margin: -455px 0px 0px 96px;
-            }
-            
-            #nav {
-                margin: -40px 0px 0px 0px;
-                width: 870px;
-                position: absolute;
-                z-index: 2;
-            }
-            
-            #nav a, #nav a:link, #nav a:active, #nav a:visited {
-                font-family: "Calibri", sans-serif;
-                font-size: 14px;
-                letter-spacing: 3px;
-                line-height: 20px;
-                color: #f4f4f4;
-                text-align: center;
-                display: block;
-                width: 100%;
-                float: left;
-                text-transform: uppercase;
-                text-decoration: none;
-                margin-top: 3px;
-            }
-            
-            #nav a:hover {
-                background-color: #424242;
-                color: #ffffff;
-                text-decoration: none;
-            }
-            
-            #nav a, #nav a:link.home, #nav a:active.home, #nav a:visited.home {
-                background-color: #bf4c4c;
-            }
-            
-            #nav a:hover.home {
-                background-color: #d14949;
-                text-decoration: none;
-            }
-            
-            #nav a, #nav a:link.about, #nav a:active.about, #nav a:visited.about {
-                background-color: #c68628;
-            }
-            
-            #nav a:hover.about {
-                background-color: #d98f22;
-                text-decoration: none;
-            }
-            
-            #nav a, #nav a:link.roster, #nav a:active.roster, #nav a:visited.roster {
-                background-color: #efea4b;
-            }
-            
-            #nav a:hover.roster {
-                background-color: #fdf744;
-                text-decoration: none;
-            }
-            
-            #nav a, #nav a:link.photos, #nav a:active.photos, #nav a:visited.photos {
-                background-color: #28c645;
-            }
-            
-            #nav a:hover.photos {
-                background-color: #1dd83f;
-                text-decoration: none;
-            }
-            
-            #nav a, #nav a:link.exco, #nav a:active.exco, #nav a:visited.exco {
-                background-color: #32b6ea;
-            }
-            
-            #nav a:hover.exco {
-                background-color: #2ac0fb;
-                text-decoration: none;
-            }
-            
-            #nav a, #nav a:link.culture, #nav a:active.culture, #nav a:visited.culture {
-                background-color: #806095;
-            }
-            
-            #nav a:hover.culture {
-                background-color: #9a6fb6;
-                text-decoration: none;
-            }
-            
-            #nav ul, #nav li {
-                list-style: none;
-            }
-            
-            #nav ul {
-                margin: 0px;
-                padding-left: 2px;
-                width: 100%;
-            }
-            
-            #nav li {
-                float: left;
-                text-align: center;
-                width: 140px;
-                padding: 0px;
-                margin: 0px 2px 0px 2px;
-            }
-            
-            #content {
-                width: 866px;
-                background-color: #727272;
-                padding: 4px 4px 2px 4px;
-                margin-top: -12px;
-            }
-            
-            #content p {
-                font-family: "Calibri", sans-serif;
-                font-size: 14px;
-                letter-spacing: 1px;
-                line-height: 18px;
-                color: #f4f4f4;
-                text-align: justify;
-                width: 862px;
-            }
-            
-            #content p.footer {
-                font-size: 12px;
-                text-align: center;
-            }
-            
-            #content a, #content a:link, #content a:active, #content a:visited {
-                color: #e0e0e0;
-                font-size: 16px;
-                font-weight: bold;
-                text-decoration: none;
-            }
-            
-            #content a:hover {
-                color: #ffffff;
-                font-weight: bold;
-                text-decoration: none;
-            }
-            
-            #content h1 {
-                width: 862px;
-                font-family: "Calibri", sans-serif;
-                font-size: 20px;
-                line-height: 18px;
-                letter-spacing: 4px;
-                padding-bottom: 2px;
-                padding-top: 2px;
-                color: #3f3f3f;
-                background-color: #b7b7b7;
-                text-align: center;
-                font-weight: normal;
-                text-transform: uppercase;
-            }
-            
-            #content h2 {
-                width: 844px;
-                font-family: "Calibri", sans-serif;
-                font-size: 18px;
-                line-height: 18px;
-                letter-spacing: 3px;
-                padding-bottom: 2px;
-                padding-top: 2px;
-                color: #c5c5c5;
-                border-bottom: solid 2px #c5c5c5;
-                text-align: left;
-                padding-left: 20px;
-                font-weight: normal;
-                text-transform: uppercase;
-            }
-        </style>
-        <!--FOR LIGHTBOX-->
-        <link rel="stylesheet" href="css/lightbox.css" type="text/css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="style.css" />
+       <!-- <!--FOR LIGHTBOX-->
+        <link rel="stylesheet" type="text/css" href="css/lightbox.css" />
         <script type="text/javascript" src="js/prototype.js">
         </script>
         <script type="text/javascript" src="js/scriptaculous.js?load=effects,builder">
         </script>
         <script type="text/javascript" src="js/lightbox.js">
         </script>
-        <!--/LIGHTBOX-->
+        <!--/LIGHTBOX
         <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+        -->
     </head>
     <body>
-        <div id="site">
-            <div id="header">
-                <div id="headerbg">
-                    <img src="word.jpg">
-                </div>
-                <div id="headerfg">
-                    <img src="pretty.png">
-                </div>
-            </div>
-            <div id="nav">
-                <ul>
-                    <li>
-                        <a href="index.php" class="home">home</a>
-                    </li>
-                    <li>
-                        <a href="about.php" class="about">about</a>
-                    </li>
-                    <li>
-                        <a href="roster.php" class="roster">roster</a>
-                    </li>
-                    <li>
-                        <a href="photos.php" class="photos">photos</a>
-                    </li>
-                    <li>
-                        <a href="exco.php" class="exco">exco</a>
-                    </li>
-                    <li>
-                        <a href="mudkips.php" class="culture">culture</a>
-                    </li>
-                </ul>
-            </div>
-            <div id="content">
+		<header>
+				<img src="word.jpg" class="bg" alt="Oberlin College Flaming Blades: Convoluti Apibus">
+				<img src="pretty.png" class="fg" alt="">
+		</header>
+		<nav>
+			<ul>
+				<li>
+					<a href="index.php" class="home">home</a>
+				</li>
+				<li>
+					<a href="about.php" class="about">about</a>
+				</li>
+				<li>
+					<a href="roster.php" class="roster">roster</a>
+				</li>
+				<li>
+					<a href="photos.php" class="photos">photos</a>
+				</li>
+				<li>
+					<a href="exco.php" class="exco">exco</a>
+				</li>
+				<li>
+					<a href="mudkips.php" class="culture">culture</a>
+				</li>
+			</ul>
+		</nav>
+		<article>
