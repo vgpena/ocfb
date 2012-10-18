7cb67ec008624f2d74971bd079cdf0fa260d32c3
Violet Peña <vpena@oberlin.edu>
2012-10-17 20:48:37 -0400

    Added photo inlines to the gallery admin.

--- ocfb/gallery/admin.py
+++ ocfb/gallery/admin.py
@@ -2,14 +2,20 @@ from django.contrib import admin
 
 from gallery.models import Gallery, Photo
 
+
+class PhotoAdmin(admin.ModelAdmin):
+	prepopulated_fields = {"slug": ("gallery", "image", "caption")}
+	list_display = ("slug", "caption", "gallery",)
+	
+class PhotoInline(admin.StackedInline):
+	model = Photo
+	prepopulated_fields = {"slug": ("gallery", "image", "caption")}
 	
 class GalleryAdmin(admin.ModelAdmin):
 	prepopulated_fields = {"slug": ("title",)}
 	list_display = ("title",)
 	
-class PhotoAdmin(admin.ModelAdmin):
-	prepopulated_fields = {"slug": ("gallery", "image", "caption")}
-	list_display = ("slug", "caption", "gallery",)
-
+	inlines = [ PhotoInline, ]
+	
 admin.site.register(Gallery, GalleryAdmin)
 admin.site.register(Photo, PhotoAdmin)
