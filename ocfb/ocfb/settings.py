1919549469f43ae0bd7c91f3beed931a7507bd7e
Violet Peña <violetpena@gmail.com>
2012-11-11 19:51:51 -0500

    Got my staticfiles working properly now, and downloaded and implemented Bootstrap.

--- ocfb/ocfb/settings.py
+++ ocfb/ocfb/settings.py
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/Users/violetpena/webdev/ocfb/ocfb/static/'
+STATIC_ROOT = '/Users/violetpena/webdev/ocfb/static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -64,6 +64,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+	'/Users/violetpena/webdev/ocfb/ocfb/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -117,7 +118,6 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
