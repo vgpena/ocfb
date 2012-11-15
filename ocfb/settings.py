d2fbe3c682f2d74008f95c880c6a546622ed36ec
Violet Peña <violetpena@gmail.com>
2012-11-15 00:55:52 -0500

    woop woop.

--- ocfb/settings.py
+++ ocfb/settings.py
@@ -1,5 +1,3 @@
-import dj_database_url
-
 # Django settings for ocfb project.
 
 DEBUG = True
@@ -11,7 +9,16 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'db.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -153,3 +160,5 @@ LOGGING = {
 }
 
 
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
